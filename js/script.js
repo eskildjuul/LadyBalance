0eb2ba705e3373b04626aca3c1c57adcdfcfcfca
eskildjuul <eskchr01@iba.dk>
2026-06-14 15:31:43 +0200

    kode færdig

--- js/script.js
+++ js/script.js
@@ -2,13 +2,15 @@ const burgerButton = document.querySelector(".burger-button");
 const siteNav = document.querySelector(".site-nav");
 
 burgerButton.addEventListener("click", function () {
-  siteNav.classList.toggle("active");
-
-  const menuIsOpen = siteNav.classList.contains("active");
+  const menuIsOpen = siteNav.classList.toggle("active");
 
   burgerButton.setAttribute("aria-expanded", menuIsOpen);
 
-  console.log("Burger-menu klikket");
-  console.log("Menu åben:", menuIsOpen);
-  console.log("aria-expanded:", burgerButton.getAttribute("aria-expanded"));
+  if (menuIsOpen) {
+    burgerButton.setAttribute("aria-label", "Luk menu");
+    console.log("Menuen er åbnet");
+  } else {
+    burgerButton.setAttribute("aria-label", "Åbn menu");
+    console.log("Menuen er lukket");
+  }
 });
